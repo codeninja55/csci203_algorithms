839aa8ca8cfc32e67aec8f913402db86c34e99de
Andrew Che <andrew@codeninja55.me>
2018-07-31 20:04:10 +1000

    Added main.c file of lab02

---
+++ lab02/c_version/main.c
@@ -0,0 +1,20 @@
+/*********************************************************************
+ * CSCI203 - Exercise 2
+ * main.c - main() for exercise 1 execution
+ * Dinh Che | dbac496 | Last modified 2018.07.31
+ * Author: codeninja55
+ *********************************************************************/
+
+#include <stdio.h>
+
+
+/* Heap implementation */
+int HEAP[];
+int CTR = 0;
+
+
+ int main()
+{
+     printf("Hello World");
+     return 0;
+}
