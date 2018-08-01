b6a74943b92beaf6d70eca9a1a9d3101e62668ff
Andrew Che <andrew@codeninja55.me>
2018-08-02 08:58:49 +1000

    Changed name of main.py to ex2.py

---
+++ lab02/py_version/ex2.py
@@ -0,0 +1,14 @@
+#######################################################################
+# CSCI203 - Exercise 1
+# ex2.c - main() for exercise 2 execution
+# Dinh Che | dbac496 | Last modified 2018.08.01
+# Author: codeninja55
+#######################################################################
+
+
+def read_file():
+
+
+
+if __name__ == "__main__":
+    filename = input("Input file name to read: ")
