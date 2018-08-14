2d6367a4e16b8add41d21cc4f8e71f497913e3ac
Andrew Che <andrew@codeninja55.me>
2018-08-14 18:09:59 +1000

    Completd impl of ex3 with efficient code

--- lab03/c_version/ex3.c
+++ lab03/c_version/ex3.c
@@ -7,7 +7,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <zconf.h>
 #include <string.h>
 #include <stdbool.h>
 
@@ -21,27 +20,29 @@ bool check(int);
 int main( int argc, const char* argv[] )
 {
     FILE *fd;
-    char filename[200];
+    char buffer[200], filename[200];
     int what, where, probe;
 
-    // display a prompt for the file name and print to stdout file to open
-    // printf("Input file name: ");
-    // scanf("%s", filename);
-    // TODO: TESTING ONLY
-    strcpy(filename, "Ex3.txt");
+    // get filename from argv or display a prompt for the file name to open file
+    if ( argc == 2 ) strcpy(filename, argv[1]);
+    else {
+        printf("Input file name: ");
+        scanf("%s", buffer);
+        strcpy(filename, buffer);
+    }
 
-    if ((fd = fopen(filename, "r")) == NULL) {
+    if ( (fd = fopen(filename, "r")) == NULL ) {
         fprintf(stderr, "[DEBUG]: Failed to open file [ %s ]\n", strerror(1));
         exit(1);
     }
 
-    while(fscanf(fd, "%d %d", &what, &where) == 2) {
+    while ( fscanf(fd, "%d %d", &what, &where) == 2 ) {
         if (ferror(fd)) break; // if the read fails, break the loop
         if (what < 0 || where < 0) break;
         // printf("%d : %d\n", what, where);
         insert(what, where);
     }
-    while(fscanf(fd, "%d", &probe) == 1) {
+    while( fscanf(fd, "%d", &probe) == 1 ) {
         if (ferror(fd) || probe < 0) break;
         printf("%6s : %d\n", check(probe) ? "TRUE" : "FALSE", DATA[probe]);
     }
