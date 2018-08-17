7ba9b7b7dbf0d9665d75f9f32022ad3a8f634639
Andrew Che <andrew@codeninja55.me>
2018-08-18 05:48:33 +1000

    Added some basic initial setup

--- assignment01/c_version/assignment01.c
+++ assignment01/c_version/assignment01.c
@@ -8,20 +8,52 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include "helper.h"
+
+#define BUFFER_SZ 200
+#define SIZE 50000
+#define WORD_SZ 50
+#define IN 1  /* inside a word */
+#define OUT 0  /* outside a word */
+typedef struct Word Word;
+
+struct Word {
+  int start_idx;
+  int end_idx;
+  int length;
+  int count;
+};
+
+// struct Node {
+//   char word[50];
+//   int count;
+// };
+
+/* Global Vars */
+char POOL[SIZE * 10];
+Word WORDS[SIZE];
+int next = 0;
 
 /* Function prototypes */
-unsigned char to_lower(char);
+char to_lower(int);
+void append(char *, int, char);
+char * build_word();
+// insert_word();
+// find_word();
 
 int main( int argc, const char* argv[] )
 {
+    printf("[DEBUG] Testing at: %s\n\n", get_current_time());
+
     FILE *fd;
-    char buffer[200], filename[200];
-    char ch;
+    char buffer[BUFFER_SZ], filename[BUFFER_SZ];
 
     // 1. Read the name of the text file from the console
     /* Implementation: if console params from console, read into filename.
      * else display a prompt for the file name and print to stdout file to open. */
-    if ( argc == 2 ) strcpy(filename, argv[1]);
+    if ( argc == 2 )
+        strcpy(filename, argv[1]);
     else {
         printf("Input file name: ");
         scanf("%s", buffer);
@@ -33,17 +65,50 @@ int main( int argc, const char* argv[] )
         exit(1);
     }
 
+    int ch, nl, nw, word_idx, cursor;
+    int state = OUT;
+    nl = nw = word_idx = cursor = 0;
+    char * new_word;
+    new_word = (char *) ec_malloc(sizeof(char) * WORD_SZ);
+
     // 2. Read in a text file, not all at once. (This can be line by line, word by word, or char by char).
-    while ( (ch = (char) fgetc(fd) ) != EOF ) {
-        fprintf(stdout, "%c", to_lower(ch));
+    while ( (ch = fgetc(fd) ) != EOF ) {
+        if ( ferror(fd) )
+            break;
+
+        if ( isalpha(ch) )
+            fprintf(stdout, "[%c]", to_lower(ch));
+
+        // 3. The file content must be converted to a sequence of words, discarding punctuation and
+        // folding all letters into lower case
+        if ( ch == '\n' ) {
+            state = OUT;
+            ++nl;
+        }
+
+        if ( ch == ' ' || ch == '\n' || ch == '\t' )
+            state = OUT;
+
+        if ( isalpha(ch) ) {
+            state = IN;
+            new_word[word_idx++] = to_lower(ch);
+        }
+
+        if ( state == OUT ) { // end of word
+            new_word[word_idx] = '\0';
+            ++nw;
+            // insert_word()
+            printf(" | %s \n", new_word);
+            free(new_word);
+            new_word = (char *) ec_malloc(sizeof(char) * WORD_SZ);
+            state = OUT;
+            word_idx = 0;
+        }
+
+        // 4. Store the unique words and maintain a count of each different word.
     }
 
     fclose(fd);
-
-    // 3. The file content must be converted to a sequence of words, discarding punctuation and
-    // folding all letters into lower case
-
-    // 4. Store the unique words and maintain a count of each different word.
 
     // 5. The words should be ordered by decreasing count and, if there are multiple words with the same count,
     // alphabetically. (This ordering may be achieved as the words are read in, partially as the words are read,
@@ -55,6 +120,13 @@ int main( int argc, const char* argv[] )
 
 }
 
+/* PRIVATE FUNCTIONS */
+
+char* build_word()
+{
+
+}
+
 /****************************************************************************
  * Title: The C Programming Language, 2nd edition
  * Author: Kernighan, B.W. & Ritchie, D.M.
@@ -62,7 +134,8 @@ int main( int argc, const char* argv[] )
  * Type: Example code.
  ****************************************************************************/
  // Wrapper function to convert a a char and coerce it from an int to a char
-unsigned char to_lower(char ch)
+// char * to_lower(char ch)
+char to_lower(int ch)
 {
-    return (unsigned char) tolower(ch);
+    return (char) tolower(ch);
 }
