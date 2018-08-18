0081f50a415f3e4b7a70a1d98eb4893cec884470
Andrew Che <andrew@codeninja55.me>
2018-08-18 16:05:37 +1000

    Added boilerplate start code for ex04

---
+++ lab04/c_version/ex4.c
@@ -0,0 +1,7 @@
+/*********************************************************************
+* Title: CSCI203 - Assignment 1
+* File: ex4.c - main() driver for exercise 4 implementation
+* Author: Dinh Che (codeninja55) andrew at codeninja.me
+* UOW Details: dbac496 | 5721970 | dbac496 at uowmail.edu.au
+* Last modified: 2018.08.18
+*********************************************************************/
---
+++ lab04/py_version/ex4.py
@@ -0,0 +1,7 @@
+#######################################################################
+# Title: CSCI203 - Exercise 4
+# File: ex4.py - main() for exercise 3 execution
+# Author: Dinh Che (codeninja55) | andrew at codeninja55.me
+# UOW Details: dbac496 | 5721970 | dbac496 at uowmail.edu.au
+# Last modified: 2018.08.14
+#######################################################################
