66106f2c59d066ec63f33fe69670162515d40a6b
Andrew Che <andrew@codeninja55.me>
2018-09-05 02:25:27 +1000

    Refactored C implem

--- ex06/c_version/ex6.c
+++ ex06/c_version/ex6.c
@@ -3,7 +3,7 @@
 * ex6.c - main() driver for implementation of Karp-Rabin string search algorithm
 * Author: Dinh Che (codeninja55) | andrew at codeninja55.me
 * UOW Details: Dinh Che (5721970) | dbac496 at uowmail.edu.au
-* Last modified: 2018.09.03
+* Last modified: 2018.09.04
 *********************************************************************/
 
 #include <string.h>
@@ -15,6 +15,9 @@
 #define BUFFER_SZ 100
 #define d 4  // universe of chars A, C, G, and T
 #define q 7  // prime number to use
+
+// 7 19 24 28 8
+//
 
 void rabin_karp_search(char[], char[]);
 
@@ -40,6 +43,7 @@ int main(int argc, const char * argv[])
     char T[5000];
     char P[10];
     fscanf(fd, "%s %s", T, P);
+    printf("\n|=====| Output |=====|\n\n");
     rabin_karp_search(T, P);
 
     fclose(fd);
@@ -50,44 +54,42 @@ int main(int argc, const char * argv[])
  * :param P: Char array pattern to match.
  * :param q: A prime number to use in hashing.
  * */
-void rabin_karp_search(char T[], char P[])
+void rabin_karp_search(char T[], char S[])
 {
-    int M = (int) strlen(P);
-    int N = (int) strlen(T);
+    int s_len = (int) strlen(S);
+    int t_len = (int) strlen(T);
     int i, j;
     int p = 0;  // hash value for pattern
     int t = 0;  // hash value for text string
-    int h = 1;
 
     // preprocessing the hash value of p
     // the hash function is pow(d, M - 1) mod q
-    for (i = 0; i < M - 1; i++)
+    int h = 1;
+    for (i = 0; i < s_len - 1; i++)
         h = (h * d) % q;
 
-    // processing the hash value of pattern and first part of search
-    for (i = 0; i < M; i++) {
-        p = (d * p + P[i]) % q;
+    // preprocessing the hash value of pattern and first part of search
+    for (i = 0; i < s_len; i++) {
+        p = (d * p + S[i]) % q;
         t = (d * t + T[i]) % q;
     }
 
     // matching
-    for (i = 0; i <= N; i++) {
+    for (i = 0; i <= t_len; i++) {
         // check the value of the current substr and iterate over chars if they match
         if (p == t) {
-            for (j = 0; j < M; j++)
-                if (T[i+j] != P[j]) break;  // if the next char doesnt match, no match found
+            for (j = 0; j < s_len; j++)
+                if (T[i+j] != S[j]) break;  // if the next char doesnt match, no match found
 
-            if (j == M)
-                printf("%-3d ", i);
+            if (j == s_len) printf("%-3d ", i);
         }
 
         // apply the hash function for the next substr by
         // removing the leading digit and adding trailing digit
-        if (i < N - M) {  // N - M because if the remaining string < M then stop
-            t = (d * (t - T[i] * h) + T[i + M]) % q;
+        if (i < t_len - s_len) {  // N - M because if the remaining string < M then stop
+            t = (d * (t - T[i] * h) + T[i + s_len]) % q;  // rolling update
 
-            // Ensure t is always positive
-            if (t < 0) t = (t + q);
+            if (t < 0) t += q;  // ensure t is always positive
         }
     }
 }
