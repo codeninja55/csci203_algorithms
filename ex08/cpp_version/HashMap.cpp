680f9742d833bc88e9b87c41ea3d13b22d15df8e
Andrew Che <andrew@codeninja55.me>
2018-10-23 10:35:34 +1100

    Completed ex8

--- ex08/cpp_version/HashMap.cpp
+++ ex08/cpp_version/HashMap.cpp
@@ -8,3 +8,138 @@
 *
 * @authors Dinh Che (codeninja55) <andrew at codeninja55.me>
 *********************************************************************************/
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+//template for generic type
+template<typename K, typename V>
+class HashNode {
+public:
+    V value;
+    K key;
+
+    //Constructor of hashnode
+    HashNode(K key, V value)
+    {
+        this->value = value;
+        this->key = key;
+    }
+};
+
+template<typename K, typename V>
+class HashMap {
+    //hash element array
+    HashNode<K, V>** arr;
+    int capacity;
+    //current size
+    int size;
+    //dummy node
+    HashNode<K, V>* dummy;
+
+public:
+    HashMap()
+    {
+        //Initial capacity of hash array
+        capacity = 20;
+        size = 0;
+        arr = new HashNode<K, V>* [capacity];
+
+        //Initialise all elements of array as NULL
+        for (int i = 0; i<capacity; i++)
+            arr[i] = NULL;
+
+        //dummy node with value and key -1
+        dummy = new HashNode<K, V>(-1, -1);
+    }
+    // This implements hash function to find index
+    // for a key
+    int hash_code(K key)
+    {
+        return key%capacity;
+    }
+
+    //Function to add key value pair
+    void insert_node(K key, V value)
+    {
+        auto* temp = new HashNode<K, V>(key, value);
+
+        // Apply hash function to find index for given key
+        int hash_index = hash_code(key);
+
+        //find next free space
+        while (arr[hash_index]!=NULL && arr[hash_index]->key!=key
+                && arr[hash_index]->key!=-1) {
+            hash_index++;
+            hash_index %= capacity;
+        }
+
+        //if new node to be inserted increase the current size
+        if (arr[hash_index]==NULL || arr[hash_index]->key==-1)
+            size++;
+        arr[hash_index] = temp;
+    }
+
+    //Function to delete a key value pair
+    V delete_node(int key)
+    {
+        // Apply hash function to find index for given key
+        int hash_index = hash_code(key);
+
+        //finding the node with given key
+        while (arr[hash_index]!=NULL) {
+            //if node found
+            if (arr[hash_index]->key==key) {
+                HashNode<K, V>* temp = arr[hash_index];
+
+                //Insert dummy node here for further use
+                arr[hash_index] = dummy;
+
+                // Reduce size
+                size--;
+                return temp->value;
+            }
+            hash_index++;
+            hash_index %= capacity;
+        }
+
+        //If not found return null
+        return NULL;
+    }
+
+    //Function to search the value for a given key
+    V get(int key)
+    {
+        // Apply hash function to find index for given key
+        int hash_index = hash_code(key);
+
+        //finding the node with given key
+        while (arr[hash_index]!=NULL) {
+            //if node found return its value
+            if (arr[hash_index]->key==key)
+                return arr[hash_index]->value;
+            hash_index++;
+            hash_index %= capacity;
+        }
+
+        //If not found return null
+        return NULL;
+    }
+
+    //Return current size
+    int size_of_map() { return size; }
+
+    //Return true if size is 0
+    bool is_empty() { return size==0; }
+
+    //Function to display the stored key value pairs
+    void display()
+    {
+        for (int i = 0; i<capacity; i++) {
+            if (arr[i]!=NULL && arr[i]->key!=-1)
+                cout << "key = " << arr[i]->key
+                     << "  value = " << arr[i]->value << endl;
+        }
+    }
+};
--- ex08/cpp_version/ex08.cpp
+++ ex08/cpp_version/ex08.cpp
@@ -9,3 +9,120 @@
 * @authors Dinh Che (codeninja55) <andrew at codeninja55.me>
 *********************************************************************************/
 
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <iomanip>
+#include "HashMap.cpp"
+using namespace std;
+
+#define MAX(a,b) ( ((a) > (b)) ? (a) : (b) )
+#define BUFFER_SZ 100
+#define CARD_SZ 52
+
+typedef struct Card {
+  char *rank{};
+  char suit{};
+
+  bool operator< ( )
+} Card;
+
+int crazy_up();
+void fatal(char *message);
+void * ec_malloc(unsigned int size);
+
+Card CARDS[CARD_SZ];
+int CTR = 0;
+
+int main(int argc, const char * argv[])
+{
+    cout << "|==========| CSCI203 Exercise 08 |==========|\nAuthor: Dinh Che (dbac496)\n\n";
+    ifstream fin;
+    char filename[BUFFER_SZ];
+
+    if (argc == 2)
+        strncpy(filename, argv[1], BUFFER_SZ);
+    else {
+        cout << "Input file name >> ";
+        cin.getline(filename, BUFFER_SZ);
+    }
+
+    fin.open(filename);
+    if(!fin.good()) {
+        cout << "[!!] Error: Failed to open file: " << filename << "\nExiting...\n";
+        return -1;
+    }
+
+    while (!fin.eof()) {
+        char tmp[3];
+        Card c;
+        fin >> tmp;
+
+        if (strlen(tmp) == 3) {
+            c.rank = (char *)ec_malloc(sizeof("10"));
+            strcpy(c.rank, "10");
+            c.suit = tmp[2];
+        } else {
+            c.rank = (char *)ec_malloc(sizeof(char));
+            *c.rank = tmp[0];
+            c.suit = tmp[1];
+        }
+        CARDS[CTR++] = c;
+    }
+
+    cout << "N cards: " << CTR - 1 << "\n";
+    cout << "Max Length: " << crazy_up() << endl;
+}
+
+int crazy_up()
+{
+    int length[CTR];
+    length[0] = 0;
+    int max_length, first_node, last_node;
+    max_length = first_node = last_node = 0;
+
+    for (int i = 1; i < CTR; i++) {
+        int len = 1;
+
+        for (int j = i-1; j >= 0; j--) {
+            if (*CARDS[i].rank == *CARDS[j].rank || CARDS[i].suit == CARDS[j].suit ||
+                *CARDS[i].rank == '8' || *CARDS[j].rank == '8') {
+                len = MAX(len, length[j] + 1);
+            }
+            if (len == max_length)
+                first_node = j;
+
+        }
+        length[i] = len;
+        if (max_length != MAX(len, max_length)) {
+            last_node = i;
+            max_length = MAX(len, max_length);
+        } else
+            max_length = MAX(max_length, len);
+    }
+
+    cout << "First Node: " << CARDS[first_node].rank << CARDS[first_node].suit << endl;
+    cout << "Last Node: " << CARDS[last_node].rank << CARDS[last_node].suit << endl;
+    return max_length;
+}
+
+// A function to display an error message and then exit
+void fatal(char * message)
+{
+    char error_message[200];
+
+    strcpy(error_message, "[!!] Fatal Error ");
+    strncat(error_message, message, 83);
+    perror(error_message);
+    exit(-1);
+}
+
+// An error-checked malloc() wrapper function
+void * ec_malloc(unsigned int size)
+{
+    void *ptr;
+    ptr = malloc(size);
+    if(ptr == nullptr)
+        fatal(const_cast<char*>("in ec_malloc() on memory allocation"));
+    return ptr;
+}
